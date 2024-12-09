497a0d2e3ef785ea69940ccdee941cd4f8cfc212
MathieuDR <mathieu@deraedt.dev>
2024-12-09 23:30:02 +0100

    🎉 Skeleton for CLI, with options!

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+  "errors"
 	"fmt"
 	"os"
+  "flag"
 	"path/filepath"
 )
 
@@ -14,14 +16,77 @@ var (
 	BuildTime = "unknown"
 )
 
+type Options struct {
+  Summary bool
+  Output string
+  SingleFile bool
+}
+
+const debugTemplate = `
+Summary? %t
+SingleFile? %t
+Output %s
+`
+
 func main() {
+  opts, optsError := parseFlags()
+  if optsError != nil{
+    fmt.Fprintln(os.Stderr, "Error:", optsError)
+    os.Exit(1)
+  }
+
+  fmt.Printf(debugTemplate, opts.Summary,  opts.SingleFile, opts.Output)
+
+  if opts.Summary {
+    printSummary()
+  }
+}
+
+func validateFlags(options Options) error{
+  if options.Output == ""{
+    return errors.New("No output option defined")
+  }
+
+  return nil
+}
+
+func parseFlags() (Options, error){
+  opts := Options{}
+
+  flag.BoolVar(&opts.Summary, "summary", false, "Show summary")
+  flag.BoolVar(&opts.Summary, "s", false, "Show summary")
+
+  flag.BoolVar(&opts.SingleFile, "single-file", true, "Single file output, default true")
+  flag.BoolVar(&opts.SingleFile, "f", true, "Single file output, default true")
+
+  flag.StringVar(&opts.Output, "o", "", "Required output destination path")
+  flag.StringVar(&opts.Output, "output", "", "Required output destination path")
+  flag.Parse()
+
+  if err := validateFlags(opts); err != nil {
+    return Options{}, err
+  }
+
+  return opts, nil
+}
+
+
+const summaryTemplate = `
+%s
+Version: %s
+Built:   %s
+Binary:  %s
+Path:    %s
+Args:    %v
+`
+
+func printSummary() {
 	exec, err := os.Executable()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error getting executable path: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Error getting executable path: ", err)
 		os.Exit(1)
 	}
 	
-	exe := filepath.Base(exec)
-
-	fmt.Printf("%s version %s (built on %s, exe is %s, exec is %s\n", ProgramName, Version, BuildTime, exe, exec)
+	bin := filepath.Base(exec)
+  fmt.Printf(summaryTemplate, ProgramName, Version, BuildTime, bin, exec, os.Args[1:])
 }
